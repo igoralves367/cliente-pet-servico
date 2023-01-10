c572bfcebfcaae58d70a2edaa55885de8abf5e62
igoralves367 <igor.alves367@gmail.com>
2023-01-10 16:36:13 -0300

    Inicia Projeto

---
+++ src/main/java/br/com/anakarolini/clientepetservico/ClientePetServicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.anakarolini.clientepetservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePetServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePetServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/anakarolini/clientepetservico/ClientePetServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anakarolini.clientepetservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePetServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
