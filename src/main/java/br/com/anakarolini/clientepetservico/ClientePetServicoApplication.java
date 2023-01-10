ca78d4a9498f276c03bb68c0567ced388d4dc35c
igoralves367 <igor.alves367@gmail.com>
2023-01-10 16:58:42 -0300

    Configura Application Java, teste Ok

--- src/main/java/br/com/anakarolini/clientepetservico/ClientePetServicoApplication.java
+++ src/main/java/br/com/anakarolini/clientepetservico/ClientePetServicoApplication.java
@@ -2,12 +2,13 @@ package br.com.anakarolini.clientepetservico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClientePetServicoApplication {
-
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		SpringApplication.run(ClientePetServicoApplication.class, args);
 	}
-
 }
