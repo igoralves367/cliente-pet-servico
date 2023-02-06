21e6abc9e76ffb5ae1f2febe4ee85d1b2a5b16b0
igoralves367 <igor.alves367@gmail.com>
2023-02-06 19:25:53 -0300

    Altera Cliente Request

--- src/main/java/br/com/anakarolini/clientepetservico/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/anakarolini/clientepetservico/cliente/application/api/ClienteRequest.java
@@ -2,7 +2,6 @@ package br.com.anakarolini.clientepetservico.cliente.application.api;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -24,7 +23,6 @@ public class ClienteRequest {
 	@NotNull
 	private LocalDate dataNascimento;
 	@CPF
-	@Column(unique = true)
 	private String cpf; 
 	@NotNull
 	private String endereco;
